d8c2dc0c27be17b350049746653d651e05ce4c98
ret7020 <ret7020@gmail.com>
2023-03-01 21:42:29 +0300

    Overfit?

--- YT_Parser/FinalBL/concat.py
+++ YT_Parser/FinalBL/concat.py
@@ -4,8 +4,9 @@ from translate import Translator
 
 translator = Translator(to_lang="ru", from_lang="en")
 res = []
-paths = [("train_q.json", "train_a.json"), ("test_q.json", "test_a.json"), ("val_q.json", "val_a.json")]
+#paths = [("train_q.json", "train_a.json"), ("test_q.json", "test_a.json"), ("val_q.json", "val_a.json")]
 
+paths = [("test_q.json", "test_a.json")]
 for pl in paths:
     questions = pl[0]
     answers = pl[1]
@@ -17,13 +18,17 @@ for pl in paths:
         answ = json.load(fd)
 
     ans = {}
+    print(len(answ))
     for i in answ:
         ans[i["question_id"]] = i["answer"]
     counter = 0
     for q in train:
-        question_trans = translator.translate(q["question"])
-        answ_trans = translator.translate(ans[q['question_id']])
-        res.append({"video_name": q["video_name"], "question": question_trans, "answer": answ_trans})
+        try: # translate to russian
+            question_trans = translator.translate(q["question"])
+            answ_trans = translator.translate(ans[q['question_id']])
+            res.append({"video_name": q["video_name"], "question": question_trans, "answer": answ_trans})
+        except: # fallback to english
+            res.append({"video_name": q["video_name"], "question": q["question"], "answer": ans[q["question_id"]]})
         counter += 1
         print(counter)
     print("Counted:", counter)
---
+++ activity_qa_dtst_load.sh
@@ -0,0 +1,7 @@
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/train_q.json
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/test_q.json
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/val_q.json
+
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/train_a.json
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/test_a.json
+!wget https://raw.githubusercontent.com/MILVLG/activitynet-qa/master/dataset/val_a.json
